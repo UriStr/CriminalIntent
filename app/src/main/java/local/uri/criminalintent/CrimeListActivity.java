84d32f73bc4a2bf65f356e2295da7442f1eee9c3
Yuri Strelnikov <yuri.strelnikov@mail.ru>
2019-01-09 12:11:34 +0300

    list added, holder, adapter, recyclerview

---
+++ app/src/main/java/local/uri/criminalintent/CrimeListActivity.java
@@ -0,0 +1,12 @@
+package local.uri.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+}
