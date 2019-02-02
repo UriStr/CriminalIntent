cf5068f5866290136c5f765dd436a86d2776a1d5
UriStr <yuri.strelnikov@mail.ru>
2019-02-02 22:05:44 +0300

    Suspect chooser functionality added

--- app/src/main/java/local/uri/criminalintent/CrimeFragment.java
+++ app/src/main/java/local/uri/criminalintent/CrimeFragment.java
@@ -2,6 +2,8 @@ package local.uri.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
@@ -206,6 +208,27 @@ public class CrimeFragment extends Fragment {
             calendar.set(Calendar.MINUTE, time[1]);
             mCrime.setDate(calendar.getTime());
             updateDate();
+        }
+
+        if (requestCode == REQUEST_CONTACT && data != null) {
+            Uri contactUri = data.getData();
+
+            String[] queryFields = new String[]{ContactsContract.Contacts.DISPLAY_NAME};
+
+            Cursor c = getActivity().getContentResolver().query(contactUri, queryFields, null, null, null);
+            try {
+                if (c.getCount() == 0) {
+                    return;
+                }
+
+                c.moveToFirst();
+
+                String suspect = c.getString(0);
+                mCrime.setSuspect(suspect);
+                mSuspectButton.setText(suspect);
+            } finally {
+                c.close();
+            }
         }
     }
     private String getCrimeReport() {
